887c87e1b11c853d742c7dfd034e2148a027e0e2
NabilRafa <nabilr.athy@gmail.com>
2026-02-11 19:09:33 +0700

    Updated compile.bat

--- compile.bat
+++ compile.bat
@@ -1,11 +1,14 @@
 @echo off
+
 pyinstaller ^
 --onefile ^
 --noconsole ^
 --icon=icon.ico ^
 --distpath Compiled ^
---workpath BuildTemp ^
+--workpath Compiled\BuildTemp ^
 Download_Lagu.py
+
+rmdir /s /q Compiled\BuildTemp
 
 echo.
 echo Build selesai. Tekan tombol apa saja untuk keluar...
